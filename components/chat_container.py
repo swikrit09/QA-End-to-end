662bd94e828addccc1d89c5bc718ce09de67ef85
Swikrit <swikki2003@gmail.com>
2025-01-30 19:22:18 +0530

    chat container added

---
+++ components/chat_container.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import time
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from utils.translation import get_language_names, translate
+
+# Main Q&A interface
+def create_chat_container(llm,prompt,type="Document"):
+    prompt1 = st.text_input(f"Enter Your Question From {type}")
+    if prompt1:
+        if st.session_state.vectors:
+            selected_embedding = st.sidebar.selectbox("Select Embedding", st.session_state.vectors.keys())
+            if selected_embedding:
+                document_chain = create_stuff_documents_chain(llm, prompt)
+                retriever = st.session_state.vectors[selected_embedding]["vectors"].as_retriever()
+                retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+                # Process the query
+                start = time.process_time()
+                if len(st.session_state.conversations) == 0 or prompt1 != st.session_state.conversations[-1]["question"]:
+                    response = retrieval_chain.invoke({'input': prompt1})
+                    response_time = time.process_time() - start
+                    # With a Streamlit expander
+                    with st.expander("Document Similarity Search"):
+                        for i, doc in enumerate(response["context"]):
+                            st.write(doc.page_content)
+                            st.write("--------------------------------")
+
+
+                    # Save the conversation
+                    st.session_state.conversations.append({"question": prompt1, "answer": response['answer'],"response_time":response_time })
+
+                # Retrieve the last response
+                last_response = st.session_state.conversations[-1]["answer"]
+                time = st.session_state.conversations[-1]['response_time']
+                # Display the response
+                st.write(f"Response Time: {time:.2f} seconds")
+                st.write(last_response)
+
+                selected_language = st.selectbox(
+                    "Translate to",
+                    (get_language_names()),
+                    index=0,  # Default to the first language in the list
+                )
+                if selected_language and selected_language != "Select":
+                    translation = translate("English", selected_language, last_response)
+                    with st.expander("Translated Response"):
+                        st.write(translation)
+
+        else:
+            st.error("No vector embeddings found. Please create one first!")
+
+
+    # Display saved conversations
+    st.subheader("Saved Conversations")
+    if st.session_state.conversations:
+        for idx, convo in enumerate(st.session_state.conversations):
+            with st.expander(f"Conversation {idx + 1}"):
+                st.write(f"**Question:** {convo['question']}")
+                st.write(f"**Answer:** {convo['answer']}")
+    else:
+        st.write("No conversations saved yet.")
--- pages/docRag.py
+++ pages/docRag.py
@@ -1,29 +1,21 @@
 import streamlit as st
-import os
 from langchain_groq import ChatGroq
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from utils.translation import get_language_names, translate
-from utils.available_model import available_models
 from components.settings_modal import settings_modal
+from components.chat_container import create_chat_container
 from dotenv import load_dotenv
-import time
-from streamlit_modal import Modal
+
 load_dotenv()
 
 st.set_page_config(page_title="Q&A with Documents", layout="wide")
-st.title("Q&A with Uploaded Documents")
+st.title("🐱‍💻🐱‍🐉Q&A with Uploaded Documents")
 
 groq_api_key = st.session_state.get("groq_api_key", None)
 google_api_key = st.session_state.get("google_api_key", None)
-
-
-
 
 
 settings_modal()
@@ -117,62 +109,7 @@ if google_api_key and groq_api_key:
     for name in st.session_state.vectors.keys():
         st.sidebar.write(f"- {name}")
 
-    # Main Q&A interface
-
-    prompt1 = st.text_input("Enter Your Question From Documents")
-    if prompt1:
-        if st.session_state.vectors:
-            selected_embedding = st.sidebar.selectbox("Select Embedding", st.session_state.vectors.keys())
-            if selected_embedding:
-                document_chain = create_stuff_documents_chain(llm, prompt)
-                retriever = st.session_state.vectors[selected_embedding]["vectors"].as_retriever()
-                retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
-                # Process the query
-                start = time.process_time()
-                if len(st.session_state.conversations) == 0 or prompt1 != st.session_state.conversations[-1]["question"]:
-                    response = retrieval_chain.invoke({'input': prompt1})
-                    response_time = time.process_time() - start
-                    # With a Streamlit expander
-                    with st.expander("Document Similarity Search"):
-                        for i, doc in enumerate(response["context"]):
-                            st.write(doc.page_content)
-                            st.write("--------------------------------")
-                            
-                            
-                    # Save the conversation
-                    st.session_state.conversations.append({"question": prompt1, "answer": response['answer'],"response_time":response_time })
-
-                # Retrieve the last response
-                last_response = st.session_state.conversations[-1]["answer"]
-                time = st.session_state.conversations[-1]['response_time']
-                # Display the response
-                st.write(f"Response Time: {time:.2f} seconds")
-                st.write(last_response)
-
-                selected_language = st.selectbox(
-                    "Translate to",
-                    (get_language_names()),
-                    index=0,  # Default to the first language in the list
-                )
-                if selected_language and selected_language != "Select":
-                    translation = translate("English", selected_language, last_response)
-                    with st.expander("Translated Response"):
-                        st.write(translation)
-
-        else:
-            st.error("No vector embeddings found. Please create one first!")
-
-
-    # Display saved conversations
-    st.subheader("Saved Conversations")
-    if st.session_state.conversations:
-        for idx, convo in enumerate(st.session_state.conversations):
-            with st.expander(f"Conversation {idx + 1}"):
-                st.write(f"**Question:** {convo['question']}")
-                st.write(f"**Answer:** {convo['answer']}")
-    else:
-        st.write("No conversations saved yet.")
+    create_chat_container(llm,prompt,"Uploaded Document")
         
 else:
     st.error("Please add the api keys in the settings")
