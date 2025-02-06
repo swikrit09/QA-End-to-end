fdf111650e7c1dbd8f26eceaf10633ef309e048e
Swikrit <swikki2003@gmail.com>
2025-02-07 00:59:15 +0530

    tried to set file seek 0

--- components/display_pdf.py
+++ components/display_pdf.py
@@ -6,6 +6,8 @@ def display_pdf(file):
 
     st.sidebar.markdown("### PDF Preview")
     base64_pdf = base64.b64encode(file.read()).decode("utf-8")
+    file.seek(0)
+    
 
     # Embedding PDF in HTML
     pdf_display = f"""<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="500" type="application/pdf"
